d0aedbd0ada99e78d24eb990ad2ce1ba186d7b7e
Priyansh Trivedi <devl.priyansh@gmail.com>
2015-04-15 12:44:52 +0530

    v1.4 Environment Setup. No analysis is being done. Yet.

--- main.py
+++ main.py
@@ -29,17 +29,31 @@ class Frame:											#Ram
 		self.size=size
 		self.location=startLocation
 		self.index=index
-		self.instruction_count = -1			#Only to be used for LRU
+		self.last_referred = -1			#Only to be used for LRU
+		self.last_wrote = -1
 		self.time_fetched = 0
 		self.permissions_read = False
 		self.permissions_write = False
 		self.ismapped = False
 
-	def isReferenced(self,instruction_count):		#Only to be used for LRU
-		self.instruction_count = instruction_count
+	def isReferenced_read(self,instruction_count):		#Only to be used for LRU
+		self.last_referred = instruction_count
 
 	def isFetched(self,instruction_count):			#Only to be used for FIFO
 		self.time_fetched = instruction_count
+
+	def isReferenced_write(self,instruction_count):
+		self.last_wrote = instruction_count
+
+	def lastAccessed(self):
+		if not self.ismapped:
+			return -1
+		if self.last_wrote == -1:
+			return self.last_referred
+		if self.last_wrote > self.last_referred:
+			return self.last_wrote
+		else:
+			return self.last_referred
 
 	def printable(self):
 		return 'ismapped: %(ismapped)s, index: %(index)s, location = %(location)s' % {'ismapped': self.ismapped, 'index': self.index, 'location': self.location}
@@ -61,7 +75,7 @@ def findPage(location):
 	#Finds a page based on an address.
 	#The page retured belongs to virtual memory.
 	global instruction_count
-	print "len" + str(len(virtualMemory))
+	#print "len" + str(len(virtualMemory))
 	for index in range(len(virtualMemory)):
 		lowerIndex = virtualMemory[index].location
 		try:
@@ -71,16 +85,9 @@ def findPage(location):
 		if int(location) >= int(lowerIndex) and int(location) < int(upperIndex):
 			return virtualMemory[index]
 
-def LRU():
-	for node in input:
-		location = int(str(node[2]),16)	#converting it into integer
-		page = findPage(location)
-		if page:
-			print "found page"
-		else:
-			print "no page found"
+##################################################ALGORITHMS######################################################################
 
-def least_recently_used(self):
+def least_recently_used():
 	global ram
 	
 	#Find first mapped page and put it in candidate var
@@ -95,20 +102,20 @@ def least_recently_used(self):
 	#By now either we did find an empty frame and threw it out to be replaced or we have a value in candidate
 
 	for frame in ram:
-		if frame.ismapped and frame.instruction_count < candidate.instruction_count :
+		if frame.ismapped and frame.lastAccessed() < candidate.lastAccessed() :
 			candidate = frame
 		else:
 			return frame
 
 	return candidate
 			
-def first_in_first_out(self):
+def first_in_first_out():
 	global ram
 
 	#Put first mapped page into candidate var
 	candidate = None
 	for frame in ram:
-		if page.ismapped:
+		if frame.ismapped:
 			candidate = frame
 		else:
 			return frame
@@ -120,6 +127,25 @@ def first_in_first_out(self):
 			candidate = frame
 
 	return candidate
+
+##################################################ENVIRONMENT#####################################################################
+
+#This function is expected to find a relevant frame, and map it with our page and make the system ready to proceed
+def page_fault(process,permissions,page):
+	global instruction_count
+
+	#Currently running FIFO
+	frame = first_in_first_out()
+	frame.permissions_read = True
+	if permissions:
+		frame.permissions_write = True
+	frame.ismapped = True
+	#Specified that the frame has been mapped to a page and its permissions have been mentioned.
+
+	page.frame_index = frame.index 			#Actual mapping
+	page.process = process 					#What process is running in the page
+	return frame
+
 
 def readFile(path):	#it takes path with file name as input
 	global input
@@ -143,12 +169,71 @@ def readFile(path):	#it takes path with file name as input
 #This class has following functions for now 
 # Read, Name of the Process , Memory Location''' 
 
-def simulate():
+def environment():
 	global virtualMemory, ram, instruction_count, input
-	#for line in input:
-		#print line #Can render the trace file perfectly
-	for frame in ram:
-		print frame.printable()
+
+	for line in input:	
+		#print line
+		page = None					#Object of page which is in the location
+		permissions = None			#True if write, else false
+		process = None
+
+		page = findPage(int(str(line[2]),16))
+		if str(line[1]).upper() == 'R':
+			permissions = True
+		elif str(line[1]).upper() == 'W':
+			permissions = False
+		else:
+			#Weird set of permissions
+			print line, "Permissions not recognized"
+			continue
+		if not page:
+			#Huh! This should not have happened. Let us ignore?
+			print line, "Memory Location Out of Bounds"
+			continue
+		try:
+			process = int(line[0])
+		except:
+			print line, "Process ID is not an ID :/"
+			continue
+		if not process.__class__ == int('3').__class__:
+			print line, "Process ID is not an ID :/"
+			continue
+
+		print line
+		print "page referenced: ", page.printable()
+		#Good to go with the three variables.
+		#Let's see if the page is mapped or not
+		if page.isMapped() and page.process == process:
+			#Cool. Better go and just update the time
+			frame = ram[page.frame_index]
+			if frame.__class__ != ram[0].__class__:
+				#Something went wrong. The page was mapped to a non existant frame
+				continue
+			frame.isReferenced_read(instruction_count)
+			if permissions:
+				frame.isReferenced_write(instruction_count)
+			print "frame found: ", frame.printable()
+
+		else:
+			#Page fault just happened.
+			frame = page_fault(process,permissions,page)
+			frame.time_fetched = instruction_count
+			#This frame will be returned with its variables appropriately set. 
+			#Need to update it's time counters now.
+			frame.isReferenced_read(instruction_count)
+			if permissions:
+				#i.e. if the page's permissions were write as well
+				frame.isReferenced_write(instruction_count)
+
+			print "frame replaced: ", frame.printable()
+			
+			#now the frame has been mapped as well as its values updated. Can continue now 
+
+		instruction_count += 1
+		#Let us go and update the referenced 
+
+
 
 if __name__ == "__main__":
 	readFile(os.path.join(traces_dir,'example_trace.txt'))
@@ -165,7 +250,7 @@ if __name__ == "__main__":
 		ram.append(frame)
 
 	#The virtual memory and RAM have been setup by now
-	simulate()
+	environment()
 # a simple function which either returns null or relevent Page object.
 
 
